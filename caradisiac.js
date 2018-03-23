d26b87a4d81f3cb98bd5bf7be346f00c622760b3
LudovicRansau <ludovic.ransau@gmail.com>
2018-03-23 17:05:35 +0100

    feat(api): First server with nodemon

--- caradisiac.js
+++ caradisiac.js
@@ -1,3 +1,11 @@
+var express = require('express')
+var app = express();
+
+// respond with "hello world" when a GET request is made to the homepage
+app.get('/', function (req, res) {
+    res.send('hello world')
+})
+
 /*
 const {getBrands} = require('node-car-api');
 
